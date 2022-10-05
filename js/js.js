b383cda258afaf4b5478d03f0880776d2b5c02c2
冷_月 <9110423+cold-month@user.noreply.gitee.com>
2022-10-05 21:59:10 +0800

    按年份统计处罚次数 YearAndTimes
    单次处罚金额分布
    MoneyDistribution

--- js/js.js
+++ js/js.js
@@ -8,6 +8,223 @@ echarts_32();
 echarts_33();
 echarts_5();
 echarts_6();
+echarts_7();
+echarts_8();
+
+//按年份统计处罚次数
+function echarts_7(){
+
+    var myChart = echarts.init(document.getElementById('echart7'));
+
+    var option = {
+        // backgroundColor: '#00265f',
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: { type: 'shadow'}
+        },
+        grid: {
+            left: '0%',
+            top:'10px',
+            right: '0%',
+            bottom: '4%',
+            containLabel: true
+        },
+        xAxis: [{
+            type: 'category',
+            data: [],
+            axisLine: {
+                show: true,
+                lineStyle: {
+                    color: "rgba(255,255,255,.1)",
+                    width: 1,
+                    type: "solid"
+                },
+            },
+
+            axisTick: {
+                show: false,
+            },
+            axisLabel:  {
+                interval: 0,
+                // rotate:50,
+                show: false,
+                splitNumber: 15,
+                textStyle: {
+                    color: "rgba(255,255,255,.6)",
+                    fontSize: '12',
+                },
+            },
+        }],
+        yAxis: [{
+            type: 'value',
+            axisLabel: {
+                //formatter: '{value} %'
+                show:true,
+                textStyle: {
+                    color: "rgba(255,255,255,.6)",
+                    fontSize: '12',
+                },
+            },
+            axisTick: {
+                show: false,
+            },
+            axisLine: {
+                show: true,
+                lineStyle: {
+                    color: "rgba(255,255,255,.1	)",
+                    width: 1,
+                    type: "solid"
+                },
+            },
+            splitLine: {
+                lineStyle: {
+                    color: "rgba(255,255,255,.1)",
+                }
+            }
+        }],
+        series: [
+            {
+
+                type: 'bar',
+                data: [],
+                barWidth:'35%', //柱子宽度
+                // barGap: 1, //柱子之间间距
+                itemStyle: {
+                    normal: {
+                        color:'#27d08a',
+                        opacity: 1,
+                        barBorderRadius: 5,
+                    }
+                }
+            }
+
+        ]
+    };
+
+    axios.get("/api/statistics/YearAndTimes")
+        .then(response => {
+            var myChart = echarts.init(document.getElementById('echart7'));
+            var data = response.data.data.integerStatistics;
+            var dataKeys = Object.keys(data);
+            for (var i = 0; i < dataKeys.length; ++i) {
+                option.series[0].data.push(data[dataKeys[i]])
+                option.xAxis[0].data.push(dataKeys[i])
+            }
+            // console.log(option);
+            myChart.setOption(option);
+            window.addEventListener("resize",function(){
+                myChart.resize();
+            });
+        })
+}
+
+//单次处罚金额分布
+function echarts_8(){
+    var myChart = echarts.init(document.getElementById('echart8'));
+
+    var option = {
+        // backgroundColor: '#00265f',
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: { type: 'shadow'}
+        },
+        grid: {
+            left: '0%',
+            top:'10px',
+            right: '0%',
+            bottom: '4%',
+            containLabel: true
+        },
+        xAxis: [{
+            type: 'category',
+            data: [],
+            axisLine: {
+                show: true,
+                lineStyle: {
+                    color: "rgba(255,255,255,.1)",
+                    width: 1,
+                    type: "solid"
+                },
+            },
+
+            axisTick: {
+                show: false,
+            },
+            axisLabel:  {
+                interval: 0,
+                // rotate:50,
+                show: false,
+                splitNumber: 15,
+                textStyle: {
+                    color: "rgba(255,255,255,.6)",
+                    fontSize: '12',
+                },
+            },
+        }],
+        yAxis: [{
+            type: 'value',
+            axisLabel: {
+                //formatter: '{value} %'
+                show:true,
+                textStyle: {
+                    color: "rgba(255,255,255,.6)",
+                    fontSize: '12',
+                },
+            },
+            axisTick: {
+                show: false,
+            },
+            axisLine: {
+                show: true,
+                lineStyle: {
+                    color: "rgba(255,255,255,.1	)",
+                    width: 1,
+                    type: "solid"
+                },
+            },
+            splitLine: {
+                lineStyle: {
+                    color: "rgba(255,255,255,.1)",
+                }
+            }
+        }],
+        series: [
+            {
+
+                type: 'bar',
+                data: [],
+                barWidth:'35%', //柱子宽度
+                // barGap: 1, //柱子之间间距
+                itemStyle: {
+                    normal: {
+                        color:'#27d08a',
+                        opacity: 1,
+                        barBorderRadius: 5,
+                    }
+                }
+            }
+
+        ]
+    };
+
+    axios.get("/api/statistics/MoneyDistribution")
+        .then(response => {
+            var myChart = echarts.init(document.getElementById('echart8'));
+            var data = response.data.data.integerStatistics;
+            var dataKeys = Object.keys(data);
+            for (var i = 0; i < dataKeys.length; ++i) {
+                option.series[0].data.push(data[dataKeys[i]])
+                option.xAxis[0].data.push(dataKeys[i])
+            }
+            // console.log(option);
+            myChart.setOption(option);
+            window.addEventListener("resize",function(){
+                myChart.resize();
+            });
+        })
+}
+
+
 function echarts_1() {
         // 基于准备好的dom，初始化echarts实例
         // var myChart = echarts.init(document.getElementById('echart1'));
@@ -124,7 +341,7 @@ function echarts_2() {
         var myChart = echarts.init(document.getElementById('echart2'));
 
         var option = {
-    //  backgroundColor: '#00265f',
+     // backgroundColor: '#00265f',
         tooltip: {
             trigger: 'axis',
             axisPointer: { type: 'shadow'}
